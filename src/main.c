0cff8ae2d5ff6dbd3f6e96d79298d09bd18f5119
kderef <kianheitkamp@outlook.com>
2025-06-25 01:34:32 +0200

    You can look around

--- src/main.c
+++ src/main.c
@@ -1,37 +1,34 @@
-// ------------------------------------------------------------------------------
-// Simple Raylib Template
-// https://github.com/itsYakub/Simple-Raylib-Template.git
-// ------------------------------------------------------------------------------
-// Author:
-// https://github.com/itsYakub
-// ------------------------------------------------------------------------------
-// LICENCE (MIT):
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
-// ------------------------------------------------------------------------------
-
 #include "raylib.h"
+#include "camera.c"
+#include <stdio.h>
+#include <stdbool.h>
+
+Camera3D camera_init() {
+    Camera3D cam = {
+        .fovy = 90,
+        .position = {1, 1, 1},
+        .projection = CAMERA_PERSPECTIVE,
+        .up = {0, 1, 0},
+        .target = {10, 1, 10},
+    };
+
+  return cam;
+}
+
+void capture_mouse(bool capture) {
+    if (capture) DisableCursor();
+    else EnableCursor();
+}
+
+
 
 int main(int argc, char** argv) {
+    // --- config
     const char* TITLE = "Simple Raylib Template";
     const int WIDTH = 1280;
     const int HEIGHT = 768;
 
+    // --- flags
     ConfigFlags config_flags =
         FLAG_MSAA_4X_HINT | 
         FLAG_WINDOW_RESIZABLE |
@@ -39,22 +36,37 @@ int main(int argc, char** argv) {
 
     SetConfigFlags(config_flags);
 
-    // Initializing resources:
-    // - main game window;
-    // - game's audio device;
+    // --- initialisation
     InitWindow(WIDTH, HEIGHT, TextFormat("Raylib %s - %s", RAYLIB_VERSION, TITLE));
     InitAudioDevice();
+    SetExitKey(KEY_NULL);
 
-    while(!WindowShouldClose()) {
-        // Update your game logic here...
+    // --- input and camera
+    Camera3D camera = camera_init();
+    capture_mouse(true);
+
+    bool running = true;
+    
+    while (running) {
+        running ^= WindowShouldClose();
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        
+        // Draw 3D
+        BeginMode3D(camera);
+        camera_look(&camera);
+        DrawGrid(1000, 10.0);
 
-        // Render your graphics here...
+        EndMode3D();
 
+        // Draw 2D on top
+
+        DrawFPS(0, 0);
         EndDrawing();
     }
+
+    capture_mouse(false);
 
     // Unloading resources...
     CloseAudioDevice();
