5842b7317d296ab55e59907b1564aff4bb8e42dd
kderef <96412362+kderef@users.noreply.github.com>
2025-06-24 18:49:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<div align="Center">
+
+# Simple Raylib Template 📜
+
+</div>
+
+## 🔎 Overview
+
+This is the simple template for raylib games and applications, written in C.
+
+## Prerequesites 📝
+- **[Git;](https://git-scm.com/)**
+- **[CMake;](https://cmake.org/)**
+
+## Setup 🛠️
+- Create a Github repository from this template or clone this repo directly from Github (up to you!);
+- In the projects directory:
+    - Create and enter your final binary directory:
+    ```console
+    $ mkdir bin/
+    $ cd bin/
+    ```
+    - Build the project using CMake...
+    ```console
+    $ cmake ..
+    $ cmake --build .
+    ```
+
+## ©️ Credits
+- **[Raylib CMake project](https://github.com/raysan5/raylib/tree/master/projects/CMake)**
+- **[github/gitignore](https://github.com/github/gitignore)**
+- **[gitattributes/gitattributes](https://github.com/gitattributes/gitattributes)**
+- **[licenses/license-templates](https://github.com/licenses/license-templates)**
+
+## Licence❕
+This template is under the [MIT Licence](https://github.com/itsYakub/Simple-Raylib-Template/blob/main/LICENCE), but you can freely modify it, change it to whatever suits your needs!
---
+++ src/main.c
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------------------------
+// Simple Raylib Template
+// https://github.com/itsYakub/Simple-Raylib-Template.git
+// ------------------------------------------------------------------------------
+// Author:
+// https://github.com/itsYakub
+// ------------------------------------------------------------------------------
+// LICENCE (MIT):
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+// ------------------------------------------------------------------------------
+
+#include "raylib.h"
+
+int main(int argc, char** argv) {
+    const char* TITLE = "Simple Raylib Template";
+    const int WIDTH = 1280;
+    const int HEIGHT = 768;
+
+    ConfigFlags config_flags =
+        FLAG_MSAA_4X_HINT | 
+        FLAG_WINDOW_RESIZABLE |
+        FLAG_VSYNC_HINT;
+
+    SetConfigFlags(config_flags);
+
+    // Initializing resources:
+    // - main game window;
+    // - game's audio device;
+    InitWindow(WIDTH, HEIGHT, TextFormat("Raylib %s - %s", RAYLIB_VERSION, TITLE));
+    InitAudioDevice();
+
+    while(!WindowShouldClose()) {
+        // Update your game logic here...
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // Render your graphics here...
+
+        EndDrawing();
+    }
+
+    // Unloading resources...
+    CloseAudioDevice();
+    CloseWindow();
+
+    return 0;
+}
