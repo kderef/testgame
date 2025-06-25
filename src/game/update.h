d16637277cf51457b32ea17bebc1329b2c9c5dd3
kderef <kianheitkamp@outlook.com>
2025-06-25 23:18:05 +0200

    rust init

--- src/game/update.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-#include "game.h"
-
-
-void game_update(game_t* game) {
-    
-}
--- src/input.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-#include <stdbool.h>
-
-void capture_mouse(bool capture) {
-    if (capture) DisableCursor();
-    else EnableCursor();
-}
